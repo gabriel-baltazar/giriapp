6108b6864f69e3a157a2f56398f211b3ca4df460
alvaroascenco <alvaroascenco16@outlook.com>
2021-11-27 22:45:36 -0300

    new idiom feature model and migration files

---
+++ api/database/migrations/2021_11_28_014125_idiom.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Idiom extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('idioms', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('criadoPor')->unsigned();
+            $table->foreign('criadoPor')->references('id')->on('users');
+
+            $table->string('expressao_pt');
+            $table->string('expressao_en');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
